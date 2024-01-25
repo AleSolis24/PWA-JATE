0cbf060af1b10a723466f415c2e33313b3d6de7e
AleSolis24 <aletitan24@icloud.com>
2024-01-24 23:16:13 -0500

    completed the last TODO for getting logic for a method to get all content from the db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,15 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-const db = await openDB('jate', 1);
-const tx = db.transaction('jate', 'readwrite');
+export const putDb = async (content) => {console.error('putDb not implemented');
+const jateDB = await openDB('jate', 1);
+const tx = jateDB.transaction('jate', 'readwrite');
 const store = tx.objectStore('jate');
 const request = store.put({ id: 1, value: content});
 const result = await request;
-console.log('Your data has been saved to the database', result.value);
-
+console.log(result.value);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {console.error('getDb not implemented');
+const jateDB = await oppenDB('jate', 1); 
+const tx = jateDB.transaction('jate', 'randonly');
+const store = tx.objectStore('jate'); 
+const request = store.get(1);
+const result = await request;
+console.log(result.value)
+return result;
+
+};
 
 initdb();
